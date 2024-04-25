264cc30ad0f115c322363ea3000cafc911ad7624
lszin777 silv <lucassilva.lima@eaportal.org>
2024-04-25 16:35:09 +0000

    Correção do erro

--- sript.js
+++ sript.js
@@ -1,11 +1,11 @@
 let botoes = document.querySelectorAll(".botoes button")
 botoes.forEach(botao => {
-    botao.addEventListener("click",)
+    botao.addEventListener("click", guardarNota)
 })
 
 function guardarNota(evento) {
     let nota = evento.target.innerText
 
     localStorage.setItem("nota", nota)
-    
+
 }
